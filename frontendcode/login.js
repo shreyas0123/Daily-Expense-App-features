18ce2156eba099d431cf59941dd5e0a6425b3ccf
shreyas0123 <shreyasgowdaab@gmailcom>
2023-07-12 15:54:19 +0530

    Creating expense app features

---
+++ frontendcode/login.js
@@ -0,0 +1,26 @@
+async function saveToCloudStoarage(event) {
+  try {
+    event.preventDefault();
+
+    const loginDetails = {
+      email: event.target.email.value,
+      password: event.target.password.value
+    }
+    console.log('loginDetails', loginDetails);
+
+    const response = await axios.post('http://localhost:700/login', loginDetails)
+    event.target.reset();
+    alert(response.data.message)
+    window.location.href = "./expensetracker.html"   //using anchor tag after login redirecting user to the page expensetracker.html
+
+  } catch (error) {
+    const errorMessage = error.message || 'An error occured';
+    const errorHtml = `
+          <div class="col-lg-6 col-lg-offset-4">
+            <p class="text-danger">${errorMessage}</p>
+          </div>
+        `;
+    document.body.innerHTML += errorHtml;
+  }
+
+}
